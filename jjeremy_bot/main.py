414919b7a73ea1ac17ad4aa79a88df3a24f9e364
Jamshid <59513380+Jamadog@users.noreply.github.com>
2021-07-09 13:16:39 +0500

    2
    2

--- jjeremy_bot/main.py
+++ jjeremy_bot/main.py
@@ -1,8 +1,9 @@
 import telebot
-
+import draw_image
+import os
 
 bot = telebot.TeleBot('1650295407:AAHKcYX4an6CrGKKyNCSjZgaDbYHo_6WvA0')
-way_saved = 'C:/Users/User/Desktop/New_Doc/wb/tg_bots/jjeremy_bot/'
+way_saved = 'C:/Users/Jamadog/Desktop/Projects/tg_bots/jjeremy_bot/'
 
 
 @bot.message_handler(commands=['start'])
@@ -26,17 +27,26 @@ def drawing_saved(message):
     try:
         file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
         downloaded_file = bot.download_file(file_info.file_path)
-        src = way_saved + file_info.file_path
-        with open(src, 'wb') as new_file:
+        img_input = way_saved + file_info.file_path
+
+        with open(img_input, 'wb') as new_file:
             new_file.write(downloaded_file)
+            new_file.close()
         bot.reply_to(message, 'Photo saved!\n'
                               'Waiting for...')
-        way_photo = way_saved + file_info.file_path
-        photo = open(way_photo, 'rb')
+
+        img_output = draw_image.draw_img(img_input)
+        img_output.save(way_saved+file_info.file_path)
+        way_img_output = way_saved+file_info.file_path
+        photo = open(way_img_output, 'rb')
         bot.send_photo(message.chat.id, photo)
+        photo.close()
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), way_img_output)
+        os.remove(path)
 
     except Exception as Ex:
-        bot.reply_to(message, Ex)
+
+        print(Ex)
 
 
 # if __name__ == '__main__':
